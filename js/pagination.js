a0bd109271f4b2a09d7309659df73fa430c658ee
Carlos <carlos97181034535@gmail.com>
2024-11-26 18:31:50 +0100

    functionality for the opinion section

--- js/pagination.js
+++ js/pagination.js
@@ -1,23 +1,48 @@
-let step = 1
-const step1 = 1;
-const step3 = 3;
+let currentStep = 1;
+const totalSteps = 3;
+
 const backButton = document.querySelector('#back-button');
 const nextButton = document.querySelector('#next-button');
-const opinionStep1 = document.querySelector('#step-1');
-const opinionStep2 = document.querySelector('#step-2');
-const opinionStep3 = document.querySelector('#step-3');
 
-nextButton.addEventListener('click', nextSection);
+visibilityButtons()
 
-function nextSection() {
-    if(step <= 1) {
-        opinionStep1.classList.remove('display-block')
-        opinionStep1.classList.add('display-none')
-
-        opinionStep2.classList.add('display-block')
-
-        backButton.classList.remove('visibility')
-        step++
+backButton.addEventListener('click', () => {
+    if(currentStep > 1) {
+        currentStep--;
+        updateOpinions()
+        visibilityButtons()
     }
-    
-}
+});
+
+nextButton.addEventListener('click', () => {
+    if(currentStep < totalSteps) {
+        currentStep++;
+        updateOpinions()
+        visibilityButtons()
+    }
+});
+
+function updateOpinions() {
+    for( let i = 1; i <= totalSteps; i++ ) {
+        const opinionDiv = document.querySelector(`#step-${i}`)
+        
+        if(i === currentStep) {
+            opinionDiv.classList.add('display-block')
+            opinionDiv.classList.remove('display-none')
+        } else {
+            opinionDiv.classList.add('display-none')
+            opinionDiv.classList.remove('display-block')
+        }
+    }
+};
+
+function visibilityButtons() {
+    if(currentStep === 1) {
+        backButton.classList.add('visibility');
+    } else if(currentStep === totalSteps) {
+        nextButton.classList.add('visibility');
+    } else {
+        backButton.classList.remove('visibility');
+        nextButton.classList.remove('visibility');
+    }
+};
